3fc84a4e92419e00eb5fc516dc6e3bb3c334dd35
Anja Istenic <aistenic1@gmail.com>
2019-02-09 11:50:57 +0100

    added tests for roll_dice method

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,29 +1,23 @@
 class Dice
-  attr_reader :current_dice_values
+  attr_reader :current_dice_values, :number_of_rolls
   def initialize
     @dice_value_options = [1, 2, 3, 4, 5, 6]
     @clear_dice_values = { die1: nil, die2: nil, die3: nil,
-                             die4: nil, die5: nil, die6: nil }
+                           die4: nil, die5: nil, die6: nil }
     @current_dice_values = @clear_dice_values
     @number_of_rolls = 0
   end
 
   def roll_dice
-    unless @number_of_rolls > 3
+    if @number_of_rolls < 3
       @current_dice_values.keys.each do |key|
         @current_dice_values[key] = @dice_value_options.sample
-        @number_of_rolls += 1
       end
+      @number_of_rolls += 1
     else
-      print("New round!")
+      puts("New round!")
       @current_dice_values = @clear_dice_values
+      @number_of_rolls = 0
     end
   end
 end
-
-
-
-
-my_game = Dice.new
-my_game.roll_dice
-print(my_game.current_dice_values)
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,5 +1,18 @@
-require 'roll_dice'
+require 'dice'
 
 describe '#roll_dice' do
-  #
+  test_game = Dice.new
+
+  it 'each dice should have a random value between 1 and 6' do
+    test_game.roll_dice
+    expected_values = [1, 2, 3, 4, 5, 6]
+    test_game.current_dice_values.values.each do |value|
+      expect(expected_values.include? value).to be true
+    end
+  end
+
+  4.times { |_x| test_game.roll_dice }
+  it 'should not allow user to roll more than three times' do
+    expect(test_game.number_of_rolls).to be <= 3
+  end
 end
