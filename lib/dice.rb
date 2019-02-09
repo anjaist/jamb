27358f7421a1ab9858d77270a3a3ab2c9ee0339f
Anja Istenic <aistenic1@gmail.com>
2019-02-10 00:32:45 +0100

    added logic that allows for each dice to be separately rolled 3 times

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,23 +1,55 @@
 class Dice
-  attr_reader :current_dice_values, :number_of_rolls
-  def initialize
-    @dice_value_options = [1, 2, 3, 4, 5, 6]
-    @clear_dice_values = { die1: nil, die2: nil, die3: nil,
-                           die4: nil, die5: nil, die6: nil }
+  attr_reader :current_dice_values
+  def initialize(use_own_dice = false)
+    @dice_value_options = [1, 2, 3, 4, 5]
+    @clear_dice_values = []
+    5.times { @clear_dice_values << { value: nil, rolls: 0 } }
     @current_dice_values = @clear_dice_values
-    @number_of_rolls = 0
+    @use_own_dice = use_own_dice
   end
 
-  def roll_dice
-    if @number_of_rolls < 3
-      @current_dice_values.keys.each do |key|
-        @current_dice_values[key] = @dice_value_options.sample
+  def first_roll
+    (0...5).each { |x| roll_one_dice(x) }
+    display_current_board
+  end
+
+  def roll_again
+    available_dice = [0, 1, 2, 3, 4]
+    while available_dice
+      puts('which dice do you want to roll again? Press Q to finish round')
+      choice_str = gets.chomp
+      choice = choice_str.to_i - 1
+      break if choice == -1 && choice_str.upcase == 'Q'
+      if available_dice.include? choice
+        roll_one_dice(choice)
+        display_current_board
+        available_dice.delete(choice) if roll_limit_reached?(choice)
+      else
+        puts 'you alredy rolled this dice 3 times'
       end
-      @number_of_rolls += 1
-    else
-      puts("New round!")
-      @current_dice_values = @clear_dice_values
-      @number_of_rolls = 0
+    end
+    puts 'round finished!' # temp line
+    @current_dice_values = @clear_dice_values
+  end
+
+  def display_current_board
+    @current_dice_values.each { |d| puts(d[:value]) }
+  end
+
+  def roll_one_dice(dice)
+    unless roll_limit_reached?(dice)
+      @current_dice_values[dice][:value] = @dice_value_options.sample
+      @current_dice_values[dice][:rolls] += 1
     end
   end
+
+  def roll_limit_reached?(dice)
+    @current_dice_values[dice][:rolls] == 3
+  end
 end
+
+# gameplay
+#
+# my_game = Dice.new
+# my_game.first_roll
+# my_game.roll_again
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,18 +1,22 @@
 require 'dice'
 
-describe '#roll_dice' do
+describe '::dice' do
   test_game = Dice.new
 
   it 'each dice should have a random value between 1 and 6' do
-    test_game.roll_dice
-    expected_values = [1, 2, 3, 4, 5, 6]
-    test_game.current_dice_values.values.each do |value|
-      expect(expected_values.include? value).to be true
+    test_game.first_roll
+    expected_values = [1, 2, 3, 4, 5]
+    test_game.current_dice_values.each do |dice|
+      expect(expected_values.include? dice[:value]).to be true
     end
   end
 
-  4.times { |_x| test_game.roll_dice }
-  it 'should not allow user to roll more than three times' do
-    expect(test_game.number_of_rolls).to be <= 3
+  random_dice = rand(0...5)
+  4.times { |_x| test_game.roll_one_dice(random_dice) }
+  it 'should not allow user to roll one dice more than three times' do
+    expect(test_game.current_dice_values[random_dice][:rolls]).to be <= 3
   end
 end
+
+# TODO: write test that game ends after all dice have been
+# rolled 3 times
