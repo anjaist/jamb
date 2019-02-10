482500903c63eff3b86d58da86c5ef7d49882405
Anja Istenic <aistenic1@gmail.com>
2019-02-10 13:20:52 +0100

    added players to game and test to check round is finished when player presses 'q'

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,33 +1,43 @@
 class Dice
-  attr_reader :current_dice_values
+  attr_reader :current_dice_values, :players
   def initialize(use_own_dice = false)
     @dice_value_options = [1, 2, 3, 4, 5]
     @clear_dice_values = []
     5.times { @clear_dice_values << { value: nil, rolls: 0 } }
     @current_dice_values = @clear_dice_values
     @use_own_dice = use_own_dice
+    @players = { 'player1' => { active: true, score: 0 },
+                 'player2' => { active: false, score: 0 } }
+    @active = 'player1'
   end
 
-  def first_roll
-    (0...5).each { |x| roll_one_dice(x) }
-    display_current_board
+  def first_roll(player)
+    if @players[player][:active]
+      (0...5).each { |x| roll_one_dice(x) }
+      @current_dice_values.each { |d| d[:rolls] += 1 }
+      display_current_board
+    else
+      puts "it's not #{player}'s turn"
+    end
   end
 
-  def roll_again
+  def roll_again(player)
     available_dice = [0, 1, 2, 3, 4]
-    while available_dice
+    while available_dice != []
       puts('which dice do you want to roll again? Press Q to finish round')
       choice_str = gets.chomp
       choice = choice_str.to_i - 1
       break if choice == -1 && choice_str.upcase == 'Q'
       if available_dice.include? choice
+        available_dice.delete(choice) if roll_limit_reached?(choice)
         roll_one_dice(choice)
         display_current_board
-        available_dice.delete(choice) if roll_limit_reached?(choice)
       else
         puts 'you alredy rolled this dice 3 times'
       end
     end
+    @players[player][:active] = false
+    update_active_player
     puts 'round finished!' # temp line
     @current_dice_values = @clear_dice_values
   end
@@ -46,10 +56,20 @@ class Dice
   def roll_limit_reached?(dice)
     @current_dice_values[dice][:rolls] == 3
   end
+
+  def update_active_player
+    if !@players['player1'][:active]
+      @players['player2'][:active] = true
+    elsif !@players['player2'][:active]
+      @players['player1'][:active] = true
+    end
+  end
 end
 
+# TODO: write begin_game/game_loop and determine_active_player
+
 # gameplay
-#
+# #
 # my_game = Dice.new
-# my_game.first_roll
-# my_game.roll_again
+# my_game.first_roll('player1')
+# my_game.roll_again('player1')
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -4,19 +4,24 @@ describe '::dice' do
   test_game = Dice.new
 
   it 'each dice should have a random value between 1 and 6' do
-    test_game.first_roll
+    test_game.first_roll('player1')
     expected_values = [1, 2, 3, 4, 5]
     test_game.current_dice_values.each do |dice|
       expect(expected_values.include? dice[:value]).to be true
     end
   end
 
-  random_dice = rand(0...5)
-  4.times { |_x| test_game.roll_one_dice(random_dice) }
   it 'should not allow user to roll one dice more than three times' do
+    random_dice = rand(0...5)
+    4.times { |_x| test_game.roll_one_dice(random_dice) }
     expect(test_game.current_dice_values[random_dice][:rolls]).to be <= 3
   end
-end
 
-# TODO: write test that game ends after all dice have been
-# rolled 3 times
+  it "player's round ends if player presses 'q'" do
+    test_game.first_roll('player1')
+    allow_any_instance_of(Object).to receive(:gets).and_return 'q'
+    test_game.roll_again('player1')
+    expect(test_game.players['player1'][:active]).to be false
+    expect(test_game.players['player2'][:active]).to be true
+  end
+end
